dc7af3696e7ca8a72dbd406e63590cb34169fe40
Muhammad Jordan <jordan.aziz18@gmail.com>
2025-02-13 19:22:20 +0700

    add the homepagefuctionaltest

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/fuctional/ChromeDriver.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package id.ac.ui.cs.advprog.eshop.fuctional;
+
+
+class ChromeDriver {
+
+}
