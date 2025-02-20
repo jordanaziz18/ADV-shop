f41fe76358f2a5aec0313d46b46798269e3bc5c5
Muhammad Jordan <jordan.aziz18@gmail.com>
2025-02-20 10:36:01 +0700

    fixing product controller

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import java.util.Arrays;
 
@@ -19,6 +20,7 @@ class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
     private ProductService productService;
 
     private Product product;
