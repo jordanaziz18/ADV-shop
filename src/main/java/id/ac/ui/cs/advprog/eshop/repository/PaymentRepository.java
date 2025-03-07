f2514d43e99ec5f3cff32519cbc2a886c8fc60bf
Muhammad Jordan <jordan.aziz18@gmail.com>
2025-03-07 16:35:22 +0700

    [RED] Add paymentrepository skeleton

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -9,22 +9,14 @@ public class PaymentRepository {
     private final List<Payment> payments = new ArrayList<>();
 
     public void addPayment(Payment payment) {
-        payments.put(payment.getId(), payment);
+     
     }
 
     public Optional<Payment> getPayment(String id) {
-        return Optional.ofNullable(payments.get(id));
+        return null;
     }
 
     public List<Payment> getAllPayments() {
-        return new ArrayList<>(payments.values());
-    }
-
-    public void save(Payment payment) {
-        payments.put(payment.getId(), payment);
-    }
-
-    public Optional<Payment> findById(String id) {
-        return Optional.ofNullable(payments.get(id));
+        return null;
     }
 }
